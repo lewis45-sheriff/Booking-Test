124da978d3a067f3e67e39c3899b95be7c313b13
LewisKipkemoiEMTH <lkipkemoi@emtechhouse.co.ke>
2026-08-20 17:47:42 +0300

    al

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME', 'clinic_booking'),
         'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'root'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
